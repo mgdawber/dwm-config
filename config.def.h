873e35b775f9caecb23f9fadd3f2c9ac7ab5ca48
Matthew Dawber <dawber@hey.com>
2021-04-16 20:10:56 +0100

    Update dwm config.

--- config.def.h
+++ config.def.h
@@ -11,24 +11,24 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int gappx     = 1;        /* gap pixel between windows */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "InputMono:size=12" };
+static const char dmenufont[]       = "InputMono:size=12";
 
-static const char col_gray1[]       = "#272822";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#cccccc";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_blue[]       = "#ebefff";
-static const char col_red[]         = "#d78787";
+static const char normbordercolor[] = "#fdf6e3";
+static const char normbgcolor[]     = "#fdf6e3";
+static const char normfgcolor[]     = "#282828";
+static const char selbordercolor[]  = "#cc241d";
+static const char selbgcolor[]      = "#282828";
+static const char selfgcolor[]      = "#fdf6e3";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_gray1, col_blue,  col_blue},
+	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor},
+	[SchemeSel]  = { selfgcolor, selbgcolor,  selbordercolor},
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "" };
+static const char *tags[] = { "₁", "₂", "₃", "₄", "₅" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -46,11 +46,11 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
-  { "|M|",      centeredmaster },
-  { ">M>",      centeredfloatingmaster },
+	{ "| 󰙀",      tile },    /* first entry is default */
+	{ "| 󱂬",      NULL },    /* no layout function means floating behavior */
+	{ "| 󰞥",      monocle },
+  { "| 󰕫",      centeredmaster },
+  { "| 󰕫",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -66,13 +66,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_blue, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "140x34", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	/* { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } }, */
+	{ MODKEY,												XK_p,			 spawn,					 SHCMD("rofi -show run") },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
